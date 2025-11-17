fa35cbbce48a0d853fe0c5793dd53b010d84a013
Hiroshi Miura <miurahr@linux.com>
2025-11-18 02:17:29 +0900

    feat: support multiple workspaces (#23)
    * feature: support multi-workspaces
    
    * feature: support workspace switcher
    
    * fix: update dependencies and remove redundant license fields
    
    Added `electron-better-web-request` and `tslib` as dependencies. Removed redundant license fields across various packages to streamline the `package-lock.json` structure. Additionally, updated dependencies nested under `nodemon`.
    
    * Update src/main.ts
    
    * Update Electron to 36.9.5 and adjust dependency versions
    
    Upgraded Electron to version 36.9.5 and updated several nested dependencies, including `fs-extra` and `jsonfile`. Removed redundant license fields to simplify the `package-lock.json` structure.
    
    * Update Electron to 39.0.0 and adjust related dependencies
    
    Upgraded Electron to version 39.0.0, updated `defaultUserAgent` string, and modified associated dependency versions in `package.json` and `package-lock.json`.
    
    * Revert build target to rpm

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,22 @@
-import { BrowserWindow, shell } from 'electron'
+import { BrowserWindow, shell, Session, OnBeforeSendHeadersListenerDetails, BeforeSendResponse } from 'electron'
+import enhanceWebRequest from 'electron-better-web-request'
+
+const defaultUserAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
+
+const enhanceSession = (session: Session) => {
+  enhanceWebRequest(session)
+  session.setUserAgent(defaultUserAgent + ' (Slack)')
+  session.webRequest.onBeforeSendHeaders(
+    (details: OnBeforeSendHeadersListenerDetails, callback: (beforeSendResponse: BeforeSendResponse) => void) => {
+      details.requestHeaders['User-Agent'] = defaultUserAgent + ' (Slack)'
+      details.requestHeaders['Referer'] = details.referrer
+      callback({
+        cancel: false,
+        requestHeaders: details.requestHeaders
+      })
+    }
+  )
+}
 
 export default class Main {
   static mainWindow: Electron.BrowserWindow | null
@@ -27,23 +45,37 @@ export default class Main {
       autoHideMenuBar: true,
       center: true,
       webPreferences: {
+        contextIsolation: true,
         nodeIntegration: true
       }
     })
-  
+
     /**
-     * Open links in the default browser
+     * Open links in the default browser except for slack.com operations.
      */
-    Main.mainWindow.webContents.setWindowOpenHandler(({url}) => {
-      void shell.openExternal(url)
-      // We need to return 'deny' in order to not open a new electron window
-      // Works like e.preventDefault()
-      return {action: 'deny'}
+    Main.mainWindow.webContents.setWindowOpenHandler(({ url }) => {
+      if (url.includes('slack.com')) {
+        // Open if URL belongs to slack.com
+        return { action: 'allow' }
+      } else {
+        // Open external links in the system's default browser
+        shell.openExternal(url)
+        return { action: 'deny' } // Deny Electron from opening new windows directly
+      }
     })
-  
+
+    // Intercept link navigation within the page
+    Main.mainWindow.webContents.on('will-navigate', (event, url) => {
+      if (!url.includes('slack.com')) {
+        event.preventDefault() // Prevent navigation
+        shell.openExternal(url) // Open in external OS browser
+        return { action: 'deny' }
+      }
+      return { action: 'allow' }
+    })
+
     Main.mainWindow.loadURL(SLACK_APP_URL, {
-      // TODO: Fix this hacky way to get around Slack's user agent check
-      userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.3'
+      userAgent: defaultUserAgent + ' (Slack)',
     })
    
     Main.mainWindow.on('closed', Main.onClose)
@@ -54,6 +86,10 @@ export default class Main {
     Main.application = app
     Main.application.on('window-all-closed', Main.onWindowAllClosed)
     Main.application.on('ready', Main.onReady)
+
+    Main.application.on('session-created', (session) => {
+      enhanceSession(session)
+    })
 
     /**
      * Define custom protocol handler. Deep linking works on packaged versions of the application ONLY
