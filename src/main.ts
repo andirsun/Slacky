f2d487ab295719afcff66863a9c6c9f483e6083e
Anderson Laverde <hola@andersonlaverde.com>
2026-03-18 16:55:16 +0000

    fix: bump Chrome version in user agent to fix browser deprecation warning (#46) (#47)

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { BrowserWindow, shell, Session, OnBeforeSendHeadersListenerDetails, BeforeSendResponse } from 'electron'
 import enhanceWebRequest from 'electron-better-web-request'
 
-const defaultUserAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
+const defaultUserAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/142.0.0.0 Safari/537.36'
 
 const enhanceSession = (session: Session) => {
   enhanceWebRequest(session)
